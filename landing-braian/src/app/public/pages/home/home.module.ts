63b2214a6cb27f67d156453f8a9770f5a3543bbf
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-04-15 04:15:35 -0300

    [ADD] Estructura básica finalizada

---
+++ landing-braian/src/app/public/pages/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { VideoGridComponent } from 'src/app/core/component/fnt/video-grid/video-grid.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    VideoGridComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
